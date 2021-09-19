4a7cd958b9b3667cf1f176e08503119e9a9c2407
GabrielRossin <gabrielfiliperossin@hotmail.com>
2021-09-19 01:09:18 -0300

    Trabalhando com Post e Get
    escrever uma servlet que atende apenas GET ou POST
    ler parâmetros da requisição dentro da servlet
    criar um formulário HTML usando as tags form e input
    enviando os dados pelo formulário através do POST
    diferença entre GET e POST

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/NovaEmpresaServlet.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet { 
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	//O método doPost só é capaz de lidar com requisições do tipo post.
+    	
+    	System.out.println("Cadastrando nova empresa");
+    	String nome_empresa = request.getParameter("nome");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>Empresa "+ nome_empresa +" cadastrada com sucesso!</body></html>");
+        
+    }
+}
